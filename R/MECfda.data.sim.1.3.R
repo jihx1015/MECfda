f30795075eaf7f871a562f79cd0fc1ec99eb2745
jihx1015 <151476255+jihx1015@users.noreply.github.com>
2024-06-28 02:19:33 -0400

    Delete R directory

--- R/MECfda.data.sim.1.3.R
+++
@@ -1,5 +0,0 @@
-#' Simulated data
-#' @name MECfda.data.sim.1.3
-#' @docType data
-#' @keywords data
-NULL
--- R/num.int.nul.R
+++
@@ -1,6 +0,0 @@
-#' @title num.int.nul
-#' @import methods
-#' @export
-#' @exportClass num.int.nul
-#' @author Heyang Ji
-setClassUnion('num.int.nul',c("numeric","integer","NULL"))
