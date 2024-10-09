b63fb73630bbce212d2ad0360f4bbbb9029dde2b
jihx1015 <151476255+jihx1015@users.noreply.github.com>
2024-10-09 11:49:43 -0400

    Delete R directory

--- R/MECfda.data.sim.1.0.R
+++
@@ -1,5 +0,0 @@
-#' Simulated data
-#' @name MECfda.data.sim.1.0
-#' @docType data
-#' @keywords data
-NULL
--- R/integrate_func1.R
+++
@@ -1,4 +0,0 @@
-integrate_func1=function(operator, e, sig){
-  re=(operator^(-1)*sin(operator*e)+e*cos(operator*e)+sig*operator*sin(operator*e))*exp(operator^2*sig/2)
-  return(as.numeric(re))
-}
